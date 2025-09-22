155380ccc6fbf9141fd46e3cb1e3894129d05d8a
Franck D'agostini <franck.dagostini@gmail.com>
2025-09-22 15:08:56 +0200

    add fish shell configuration

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,17 @@
+set -x NVIM_APPNAME editorconfig
+set -x EDITOR nvim
+set -x AWS_REGION eu-west-3
+
+set -g fish_greeting
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# opencode
+fish_add_path /home/franck/.opencode/bin
+fish_add_path /home/franck/.pulumi/bin
+fish_add_path /home/franck/bin
+
+mise activate fish | source
+
